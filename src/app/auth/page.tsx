95760cf963c7932b77417fc8acdfaa689e23b13a
Nikita <musatovla3xnikita@gmail.com>
2024-11-21 23:52:16 +0300

    fixing bugs and adeding sass. Also i made stylling form login

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from "react";
-
+import Image from "next/image";
+import "./style.scss";
 export default function Home() {
     const [isLogin, setLoginForm] = useState(true);
     const [formData, setFormData] = useState({
@@ -37,16 +38,26 @@ export default function Home() {
     }
 
     return (
-        <div>
+        <div className = "auth-content">
             <h1>{isLogin ? "Login" : "Registration"}</h1>
-            {isLogin ? <LoginForm formData={formData}
-                handleChange={handleChange}
-                handleSubmit={handleSubmit} /> :
-                <RegisterForm
-                    formData={formData}
-                    handleChange={handleChange}
-                    handleSubmit={handleSubmit} />}
-
+            <div className="auth-container">
+                <div className="auth__image">
+                    <Image src="/river-blue.jpg"
+                        alt="background-photo"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+                <div className="form-container">
+                    {isLogin ? <LoginForm formData={formData}
+                        handleChange={handleChange}
+                        handleSubmit={handleSubmit} /> :
+                        <RegisterForm
+                            formData={formData}
+                            handleChange={handleChange}
+                            handleSubmit={handleSubmit} />}
+                </div>
+            </div>
             <button onClick={toggleForm}>
                 {isLogin ? "Перейти к регистрации" : "Перейти к логину"}
             </button>
@@ -57,23 +68,65 @@ export default function Home() {
 
 const RegisterForm = ({ formData, handleSubmit, handleChange }: any) => {
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Username" value={formData.username} onChange={handleChange} />
-            <input type="password" placeholder="Password" value={formData.password} onChange={handleChange} />
-            <input type="password" placeholder="Confirm Password" value={formData.configPassword} onChange={handleChange} />
-            <input type="email" placeholder="Email" value={formData.email} onChange={handleChange} />
-            <input type="text" placeholder="Phone" value={formData.phone} onChange={handleChange} />
+        <form className="auth-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                name="username" // Указываем имя поля
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+            />
+            <input
+                type="password"
+                name="password" // Указываем имя поля
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+            />
+            <input
+                type="password"
+                name="configPassword" // Указываем имя поля
+                placeholder="Confirm Password"
+                value={formData.configPassword}
+                onChange={handleChange}
+            />
+            <input
+                type="email"
+                name="email" // Указываем имя поля
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                name="phone" // Указываем имя поля
+                placeholder="Phone"
+                value={formData.phone}
+                onChange={handleChange}
+            />
             <button type="submit">Submit</button>
         </form>
-    )
-}
+    );
+};
 
 const LoginForm = ({ formData, handleSubmit, handleChange }: any) => {
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Username" value={formData.username} onChange={handleChange} />
-            <input type="password" placeholder="Password" value={formData.password} onChange={handleChange} />
+        <form className="auth-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                name="username" // Указываем имя поля
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+            />
+            <input
+                type="password"
+                name="password" // Указываем имя поля
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+            />
             <button type="submit">Login</button>
         </form>
-    )
-}
+    );
+};
