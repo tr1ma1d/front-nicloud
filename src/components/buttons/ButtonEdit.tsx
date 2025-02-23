61855dd7e63624c1cf5b01cd01ef3f3884d7636a
nikita <musatovla3xnikita@gmail.com>
2025-02-23 22:58:56 +0300

    adding style on button

--- src/components/buttons/ButtonEdit.tsx
+++ src/components/buttons/ButtonEdit.tsx
@@ -12,7 +12,7 @@ const ButtonEdit = () => {
     }
     return (
         <div className="absolute right-5 bottom-10 z-10">
-            <button onClick = {openEditor}className='w-12 h-12 bg-blue-500 rounded-full flex justify-center align-center hover:bg-sky-100 transition-colors'>
+            <button onClick = {openEditor}className='w-12 h-12 bg-white rounded-full flex justify-center align-center hover:bg-sky-100 transition-colors'>
                 <Image
                     src="/pen-solid.svg"
                     alt="Edit icon"
